195a20c845841a071b1161bbeaa9de32ba9c1072
Navneet Tripathi <tnavneet751@gmail.com>
2023-09-21 10:21:06 +0530

    Adding monitoring support for the application using javamelody & spring-actuators

--- src/main/java/com/navneet/restfulwebservices/entities/User.java
+++ src/main/java/com/navneet/restfulwebservices/entities/User.java
@@ -2,13 +2,20 @@ package com.navneet.restfulwebservices.entities;
 
 import java.time.LocalDate;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 import jakarta.validation.constraints.Past;
 import jakarta.validation.constraints.Size;
 
 public class User {
+	@JsonProperty("user_id")
 	private Long userId;
+	
+	@JsonProperty("user_name")
 	@Size(min = 2, message = "Username can't be empty!")
 	private String userName;
+	
+	@JsonProperty("birth_date")
 	@Past(message = "User birth date should be in the past!")
 	private LocalDate userBirthDate;
 	
